cdd9df0d3275beb37437615be9c4681b56299e00
Priyal2807 <priyalsobti@gmail.com>
2020-05-28 20:20:48 +0530

    changes made for real time data

--- app.js
+++ app.js
@@ -6,24 +6,43 @@ function renderList(doc) {
   let li = document.createElement('li');
   let name = document.createElement('span');
   let city = document.createElement('span');
+  let cross = document.createElement('div');
   li.setAttribute('data-id', doc.id);
   name.textContent = doc.data().name;
   city.textContent = doc.data().city;
+  cross.textContent = 'x';
   li.appendChild(name);
   li.appendChild(city);
+  li.appendChild(cross);
   cafeList.appendChild(li);
-  count++;
-  if (count === 1) {
-    $('li').css('border-radius', '0px 120px 0px 0px');
-  }
+  // count++;
+  // if (count === 1) {
+  //   $('div').css('border-radius', '0px 120px 0px 0px');
+  //   $('li').css('border-radius', '0px 120px 0px 0px');
+  // }
+
+  //deleting data
+  cross.addEventListener('click', (e) => {
+    e.stopPropagation();
+    let id = e.target.parentElement.getAttribute('data-id');
+    //this gets only one doc
+    db.collection('cafes').doc(id).delete();
+  });
 }
 //getting data
-db.collection('cafes').get().then((snapshot) => {
-  snapshot.docs.forEach(doc => {
-    renderList(doc);
-  })
+//this gets all the data
+//.where().get() helps in querying
+//.where('city','==','del').get()
+//orderBy('city')
+//.where().orderBy().get()
+// db.collection('cafes').orderBy('city').get().then((snapshot) => {
+//   snapshot.docs.forEach(doc => {
+//     renderList(doc);
+//   })
+//
+// });
 
-});
+
 //sending data
 form.addEventListener('submit', (e) => {
   e.preventDefault();
@@ -33,4 +52,17 @@ form.addEventListener('submit', (e) => {
   });
   form.name.value = '';
   form.city.value = '';
+});
+
+//real-time database
+db.collection('cafes').orderBy('city').onSnapshot(snapshot => {
+  let changes = snapshot.docChanges();
+  changes.forEach(change => {
+    if (change.type == 'added') {
+      renderList(change.doc);
+    } else if (change.type == 'removed') {
+      let li = cafeList.querySelector('[data-id=' + change.doc.id + ']');
+      cafeList.removeChild(li);
+    }
+  });
 });
